2ae466d66bbb60edaab37a00aa878bf211dd07b8
M7modElhalaq <mahmoudvee23@gmail.com>
2023-08-02 15:59:52 +0300

    create splash screen with routes and images

---
+++ lib/features/splash/presentation/views/splash_screen.dart
@@ -0,0 +1,51 @@
+import 'package:avatar_course2_5/core/resources/manager_assets.dart';
+import 'package:flutter/material.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    print('Hello I am init state');
+    // Future.delayed(Duration(seconds: Constants.splashTime), () {
+    //   Navigator.pushReplacementNamed(context, Routes.mainScreen);
+    // });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(ManagerAssets.splash_screen_background),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Stack(
+          children: [
+            // Center(
+            //   child: Image(
+            //     image: AssetImage(ManagerAssets.splash_screen_background2),
+            //   ),
+            // ),
+            Center(
+              child: Image(
+                image: AssetImage(ManagerAssets.logo),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
