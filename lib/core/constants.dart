e29751a50bbab7defa6e2be00dea990c59e9346d
M7modElhalaq <mahmoudvee23@gmail.com>
2023-08-05 16:51:16 +0300

    formart out boarding

--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,4 +1,9 @@
 class Constants {
-  static const double appBarElevation = 0;
+  static const double elevationZero = 0;
+  static const double baseButtonElevationZero = 0;
   static const int splashTime = 3;
+
+  static const int backButtonDuration = 300;
+  static const int baseButtonFirstSpacerFlex = 5;
+  static const int baseButtonLastSpacerFlex = 5;
 }
--- lib/features/out_boarding/presentation/views/out_boarding_screen.dart
+++ lib/features/out_boarding/presentation/views/out_boarding_screen.dart
@@ -9,7 +9,10 @@ import 'package:avatar_course2_5/core/resources/manager_width.dart';
 import 'package:flutter/material.dart';
 
 import '../../../../core/constants.dart';
+import '../../../../core/routes.dart';
 import '../../../../core/widgets/base_button.dart';
+import '../../../../core/widgets/base_button.dart';
+import '../../../../core/widgets/navigator_push_replacement.dart';
 import '../widgets/out_boarding_widget.dart';
 import '../widgets/progress_indicator.dart';
 
@@ -42,32 +45,32 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
     return Scaffold(
       appBar: AppBar(
         leading: Visibility(
-          visible: _currentPageIndex != 0,
+          visible: _isNotFirstPage(),
           child: IconButton(
             onPressed: () {
               _pageController.previousPage(
-                duration: Duration(milliseconds: 300),
+                duration: const Duration(milliseconds: 300),
                 curve: Curves.fastLinearToSlowEaseIn,
               );
             },
-            icon: Icon(Icons.arrow_back),
+            icon: const Icon(Icons.arrow_back),
           ),
         ),
         backgroundColor: ManagerColors.transparent,
-        elevation: Constants.appBarElevation,
+        elevation: Constants.baseButtonElevationZero,
         actions: [
           Container(
             width: ManagerWidth.w100,
             height: ManagerHeights.h40,
-            margin: EdgeInsetsDirectional.only(
+            margin: const EdgeInsetsDirectional.only(
               end: ManagerWidth.w12,
             ),
             child: Visibility(
-              visible: _currentPageIndex != 3,
-              child: BaseButton(
+              visible: _isNotLastPage(),
+              replacement: BaseButton(
                 width: ManagerWidth.w10,
                 height: ManagerHeights.h10,
-                title: 'Next',
+                text: ManagerStrings.start,
                 textStyle: TextStyle(
                   color: ManagerColors.black,
                   fontSize: ManagerFontSizes.s16,
@@ -76,8 +79,23 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
                 bgColor: ManagerColors.transparent,
                 elevation: 0,
                 onPressed: () {
+                  Navigator.pushReplacementNamed(context, Routes.authView);
+                },
+              ),
+              child: BaseButton(
+                width: ManagerWidth.w10,
+                height: ManagerHeights.h10,
+                text: ManagerStrings.next,
+                textStyle: TextStyle(
+                  color: ManagerColors.black,
+                  fontSize: ManagerFontSizes.s16,
+                ),
+                isVisibleIcon: false,
+                bgColor: ManagerColors.transparent,
+                elevation: Constants.baseButtonElevationZero,
+                onPressed: () {
                   _pageController.nextPage(
-                    duration: const Duration(milliseconds: 300),
+                    duration: Duration(milliseconds: Constants.backButtonDuration),
                     curve: Curves.easeIn,
                   );
                 },
@@ -87,7 +105,7 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
         ],
       ),
       body: Container(
-        margin: EdgeInsets.symmetric(
+        margin: const EdgeInsets.symmetric(
           horizontal: ManagerWidth.w30,
           vertical: ManagerHeights.h24,
         ),
@@ -105,7 +123,6 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
                     _currentPageIndex = value;
                     _initialPage = value;
                   });
-                  print(_initialPage);
                   duration:
                   const Duration(
                     milliseconds: 300,
@@ -135,88 +152,86 @@ class _OutBoardingScreenState extends State<OutBoardingScreen> {
                 ],
               ),
             ),
-            SizedBox(
+            const SizedBox(
               height: ManagerHeights.h24,
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 progressIndicator(
-                  color: _initialPage == 0
+                  color: _isFirstPage()
                       ? ManagerColors.black
                       : ManagerColors.progressIndicatorColor,
-                  width: _initialPage == 0
-                      ? ManagerWidth.w20
-                      : ManagerWidth.w8,
+                  width: _initialPage == 0 ? ManagerWidth.w20 : ManagerWidth.w8,
                 ),
                 progressIndicator(
-                  color: _initialPage == 1
+                  color: _isSecondPage()
                       ? ManagerColors.black
                       : ManagerColors.progressIndicatorColor,
-                  width: _initialPage == 1
-                      ? ManagerWidth.w20
-                      : ManagerWidth.w8,
+                  width: _initialPage == 1 ? ManagerWidth.w20 : ManagerWidth.w8,
                 ),
                 progressIndicator(
-                  color: _initialPage == 2
+                  color: _isThirdPage()
                       ? ManagerColors.black
                       : ManagerColors.progressIndicatorColor,
-                  width: _initialPage == 2
-                      ? ManagerWidth.w20
-                      : ManagerWidth.w8,
+                  width: _initialPage == 2 ? ManagerWidth.w20 : ManagerWidth.w8,
                 ),
                 progressIndicator(
-                  color: _initialPage == 3
+                  color: _isLastPage()
                       ? ManagerColors.black
                       : ManagerColors.progressIndicatorColor,
-                  width: _initialPage == 3
-                      ? ManagerWidth.w20
-                      : ManagerWidth.w8,
+                  width: _initialPage == 3 ? ManagerWidth.w20 : ManagerWidth.w8,
                 ),
               ],
             ),
-            SizedBox(
+            const SizedBox(
               height: ManagerHeights.h24,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: ManagerWidth.w40,
+            BaseButton(
+              text: ManagerStrings.start,
+              width: ManagerWidth.w64,
+              height: ManagerHeights.h50,
+              isVisibleIcon: false,
+              textStyle: TextStyle(
+                fontWeight: ManagerFontWeight.regular,
+                fontSize: ManagerFontSizes.s16,
+                color: ManagerColors.white,
               ),
-              child: ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: ManagerColors.primaryColor,
-                    minimumSize: Size(ManagerWidth.w64, ManagerHeights.h50)),
-                child: Row(
-                  children: [
-                    Spacer(
-                      flex: 5,
-                    ),
-                    Text(
-                      ManagerStrings.start,
-                      style: TextStyle(
-                        fontWeight: ManagerFontWeight.regular,
-                        fontSize: ManagerFontSizes.s16,
-                        color: ManagerColors.white,
-                      ),
-                    ),
-                    Icon(
-                      Icons.arrow_forward,
-                      color: ManagerColors.white,
-                    ),
-                    Spacer(
-                      flex: 4,
-                    ),
-                  ],
-                ),
-              ),
+              onPressed: () {
+                print('Button Start');
+                Navigator.pushReplacementNamed(context, Routes.authView);
+              },
             ),
-            SizedBox(
+            const SizedBox(
               height: ManagerHeights.h20,
             ),
           ],
         ),
       ),
     );
+  }
+
+  bool _isNotFirstPage() {
+    return _currentPageIndex != 0;
+  }
+
+  bool _isFirstPage() {
+    return _currentPageIndex == 0;
+  }
+
+  bool _isSecondPage() {
+    return _currentPageIndex == 1;
+  }
+
+  bool _isThirdPage() {
+    return _currentPageIndex == 2;
+  }
+
+  bool _isLastPage() {
+    return _currentPageIndex == 3;
+  }
+
+  bool _isNotLastPage() {
+    return _currentPageIndex != 3;
   }
 }
